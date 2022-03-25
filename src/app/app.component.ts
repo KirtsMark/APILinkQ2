c0021fe32a62d8a4bc0686fc9387f0f6cf5ea25d
KirtsMark <77389633+KirtsMark@users.noreply.github.com>
2022-03-25 21:09:48 +0700

    commit Q1IA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { PostService } from './post.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'APILinkQ2';
+  data:any;
+  constructor(private post:PostService){}
+  ngOnInit(){
+    this.post.getPosts().subscribe((result)=>{
+      console.warn("result",result)
+      this.data=result
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PostService } from './post.service'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+    {path: 'posts', component: AppComponent },
+  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
